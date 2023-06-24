6cf5e3fcbf558c24fd1f10d242197535e2a237ba
AliShakoori89 <53476225+AliShakoori89@users.noreply.github.com>
2023-06-24 14:21:44 +0330

    add main page

--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -2,9 +2,6 @@ import 'package:fanar_sign/const/datetime_now.dart';
 import 'package:fanar_sign/const/fanaar_logo.dart';
 import 'package:fanar_sign/const/profile_pic.dart';
 import 'package:flutter/material.dart';
-import 'package:persian_number_utility/persian_number_utility.dart';
-import 'package:shamsi_date/shamsi_date.dart';
-import 'package:intl/intl.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -15,99 +12,137 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
 
+  var selectedIndex = 2;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
         children: [
-          Container(
-            width: double.infinity,
-            height: MediaQuery.of(context).size.height / 4,
-            decoration: const BoxDecoration(
-              boxShadow: [
-                BoxShadow(
-                    color: Color.fromRGBO(21,70,160, 1),
-                    spreadRadius: 1,
-                    blurRadius: 15
-                )
-              ],
-              gradient: LinearGradient(
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
-                colors: [
-                  Color.fromRGBO(5,101,178, 1),
-                  Color.fromRGBO(21,70,160, 1),
-                ],
-              ),
-              borderRadius: BorderRadius.only(
-                bottomRight: Radius.circular(45)
-              )
-            ),
+          Expanded(
+            flex: 1,
             child: Container(
-              margin: const EdgeInsets.all(
-                30
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      FanaarLogo(),
-                      ProfilePic()
-                    ],
-                  ),
-                  SizedBox(
-                    height: MediaQuery.of(context).size.height / 80
-                  ),
-                  DateTimeNow()
+              width: double.infinity,
+              height: MediaQuery.of(context).size.height / 4,
+              decoration: const BoxDecoration(
+                boxShadow: [
+                  BoxShadow(
+                      color: Color.fromRGBO(21,70,160, 1),
+                      spreadRadius: 1,
+                      blurRadius: 15
+                  )
                 ],
+                gradient: LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  colors: [
+                    Color.fromRGBO(5,101,178, 1),
+                    Color.fromRGBO(21,70,160, 1),
+                  ],
+                ),
+                borderRadius: BorderRadius.only(
+                  bottomRight: Radius.circular(45)
+                )
+              ),
+              child: Container(
+                margin: const EdgeInsets.all(
+                  30
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        FanaarLogo(),
+                        ProfilePic()
+                      ],
+                    ),
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height / 80
+                    ),
+                    DateTimeNow()
+                  ],
+                ),
               ),
             ),
           ),
-          Container(
-            margin: const EdgeInsets.all(
-                30
-            ),
-            child: Row(
-              children: <Widget>[
-                Expanded(
-                  flex: 7,
-                  child: Center(
-                    child: AspectRatio(
-                      aspectRatio: 1.0,
-                      child: Container(
-                        decoration: BoxDecoration(
-                          gradient: const LinearGradient(
-                            begin: Alignment.topLeft,
-                            end: Alignment.bottomRight,
-                            colors: [
-                              Color.fromRGBO(5,101,178, 1),
-                              Color.fromRGBO(21,70,160, 1),
-                            ],
-                          ),
-                            border: Border.all(color: Colors.black, width: 3.0)),
-                      ),
+          Expanded(
+              flex: 3,
+              child: Column(
+                children: [
+                  SizedBox(height: 20,),
+                  Expanded(
+                    flex: 1,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        SizedBox(width: 15,),
+                        ListView.separated(
+                            separatorBuilder: (context, index) => const SizedBox(width: 15,),
+                            scrollDirection: Axis.horizontal,
+                            shrinkWrap: true,
+                            itemCount: 2,
+                            itemBuilder: (BuildContext context, int index){
+                              return Container(
+                                height: MediaQuery.of(context).size.height / 7,
+                                width: MediaQuery.of(context).size.width / 4,
+                                decoration: BoxDecoration(
+                                  boxShadow: const [
+                                    BoxShadow(color: Color.fromRGBO(139, 139, 139, 0.5),
+                                        spreadRadius: 1,
+                                        blurRadius: 15,
+                                      offset: Offset(
+                                        5.0, // Move to right 5  horizontally
+                                        5.0, // Move to bottom 5 Vertically
+                                      ),),
+                                    // BoxShadow(color: Color.fromRGBO(139, 139, 139, 1.0),
+                                    //     spreadRadius: 0.5,
+                                    //     blurRadius: 15,
+                                    //     offset: Offset(0, 1)),
+                                    // BoxShadow(color: Color.fromRGBO(139, 139, 139, 1.0),
+                                    //     spreadRadius: 1,
+                                    //     blurRadius: 15,
+                                    //     offset: Offset(-1, -1)),
+                                    // BoxShadow(color: Color.fromRGBO(139, 139, 139, 1.0),
+                                    //     spreadRadius: 0.5,
+                                    //     blurRadius: 15,
+                                    //     offset: Offset(-1, 1)),
+                                  ],
+                                  gradient: index/2 == 1 || index/2 == 0
+                                      ? LinearGradient(
+                                    begin: Alignment.topLeft,
+                                    end: Alignment.bottomRight,
+                                    colors: [
+                                      Color.fromRGBO(5,101,178, 1),
+                                      Color.fromRGBO(21,70,160, 1),
+                                    ],
+                                  )
+                                      : LinearGradient(
+                                    begin: Alignment.topLeft,
+                                    end: Alignment.bottomRight,
+                                    colors: [
+                                      Color.fromRGBO(255, 255, 255, 1.0),
+                                      Color.fromRGBO(232, 229, 229, 1.0),
+                                    ],
+                                  ) ,
+                                  borderRadius: BorderRadius.circular(25),
+                                ),
+                              );
+                            }
+                        ),
+                      ],
                     ),
                   ),
-                ),
-                const SizedBox(width: 12.0),
-                Expanded(
-                  flex: 7,
-                  child: Center(
-                    child: AspectRatio(
-                      aspectRatio: 1.0,
+                  SizedBox(height: 15,),
+                  Expanded(
+                    flex: 4,
                       child: Container(
-                        decoration: BoxDecoration(
-                            border: Border.all(color: Colors.black, width: 3.0)),
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            )
-          )
+                    color: Colors.red,
+                  ))
+                ],
+              ))
         ],
       ),
     );
