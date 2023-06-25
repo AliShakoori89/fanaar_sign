2908ca327f404b36a59eb2b4b74c887c0e072185
AliShakoori89 <53476225+AliShakoori89@users.noreply.github.com>
2023-06-25 12:56:33 +0330

    add card and change main page

--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -43,24 +43,46 @@ class _HomePageState extends State<HomePage> {
                           Expanded(
                             flex: 2,
                             child: Container(
-                              margin: EdgeInsets.only(
+                              margin: const EdgeInsets.only(
                                 left: 10,
                                 right: 10,
                               ),
                               decoration: BoxDecoration(
                                 borderRadius: BorderRadius.circular(25),
-                                image: DecorationImage(
+                                image: const DecorationImage(
                                   fit: BoxFit.fill,
                                   image: AssetImage("assets/image/main page image.png")
                                 )
                               ),
                               )),
-                          const SizedBox(height: 15,),
+                          const SizedBox(height: 10,),
                           Expanded(
                             flex: 7,
-                            child: Container(
-                              color: Colors.white,
-                            ),
+                            child: ListView.builder(
+                                itemBuilder: (BuildContext context, int index){
+                                  return Container(
+                                    height: MediaQuery.of(context).size.height / 12,
+                                    margin: const EdgeInsets.only(
+                                      right: 15,
+                                      left: 15,
+                                      bottom: 10
+                                    ),
+                                    decoration: BoxDecoration(
+                                      color: Colors.white,
+                                      borderRadius: BorderRadius.circular(25),
+                                      boxShadow: const [
+                                        BoxShadow(color: Color.fromRGBO(139, 139, 139, 0.4),
+                                          spreadRadius: 1,
+                                          blurRadius: 4,
+                                          offset: Offset(
+                                            0.0, // Move to right 5  horizontally
+                                            5.0, // Move to bottom 5 Vertically
+                                          ),),
+                                      ],
+                                    ),
+                                  );
+                                },
+                            itemCount: 3),
                           )
                         ],
                       ))
