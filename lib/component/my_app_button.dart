da1babe8f851216c2ffc1b2bd1911100ed1d5c8e
AliShakoori89 <53476225+AliShakoori89@users.noreply.github.com>
2023-06-28 11:26:28 +0330

    change required argument to optional argument in app button

--- lib/component/my_app_button.dart
+++ lib/component/my_app_button.dart
@@ -1,30 +1,62 @@
-import 'package:fanar_sign/const/app%20color/app_color.dart';
+import 'package:fanar_sign/const/app_color.dart';
+import 'package:fanar_sign/page/otpcode_page.dart';
 import 'package:flutter/material.dart';
 
 class MyAppButton extends StatelessWidget {
-  const MyAppButton({super.key});
+  MyAppButton({super.key, this.nationalCodeController, this.mobileNumberController, this.otpCodController});
+
+  TextEditingController? nationalCodeController;
+  TextEditingController? mobileNumberController;
+  TextEditingController? otpCodController;
+  String pattern = r'(^\+?09[0-9]{9}$)';
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.only(
-          left: MediaQuery.of(context).size.width / 20,
-          right: MediaQuery.of(context).size.width / 20,
-          bottom: MediaQuery.of(context).size.width / 20),
-      height: MediaQuery.of(context).size.height / 13,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(25),
-        gradient: const LinearGradient(
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
-          colors: AppColors.mainColor,
+    return GestureDetector(
+      child: Container(
+        margin: EdgeInsets.only(
+            left: MediaQuery.of(context).size.width / 20,
+            right: MediaQuery.of(context).size.width / 20,
+            bottom: MediaQuery.of(context).size.width / 20),
+        height: MediaQuery.of(context).size.height / 13,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(25),
+          gradient: const LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: AppColors.mainColor,
+          ),
+        ),
+        child: Center(
+          child: Text("مرحله بعد",
+              style: TextStyle(color: Colors.white,
+                  fontSize: MediaQuery.of(context).size.width / 20)),
         ),
       ),
-      child: Center(
-        child: Text("مرحله بعد",
-            style: TextStyle(color: Colors.white,
-                fontSize: MediaQuery.of(context).size.width / 20)),
-      ),
+      onTap: (){
+        RegExp regExp = RegExp(pattern);
+
+        if (nationalCodeController!.text.length == 10) {
+          if (regExp.hasMatch(mobileNumberController!.text)) {
+
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => OTPCodePage(mobileNumberController: int.parse(mobileNumberController!.text))),
+            );
+
+          } else {
+            ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                content:
+                Text('شماره موبایل وارد شده صحیح نمی باشد')));
+          }
+        }
+        else {
+          ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+              content:
+              Text('کد ملی وارد شده صحیح نمی باشد')));
+        }
+
+      },
     );
   }
 }
