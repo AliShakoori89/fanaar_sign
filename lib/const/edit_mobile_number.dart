8efda3cfb6c39ba8c1a810c8cc94f19358f33ab8
AliShakoori89 <53476225+AliShakoori89@users.noreply.github.com>
2023-06-28 11:25:05 +0330

    add mobile editor text

---
+++ lib/const/edit_mobile_number.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class EditMobileNumber extends StatelessWidget {
+  const EditMobileNumber({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(
+          top: MediaQuery.of(context).size.height / 30,
+          left: MediaQuery.of(context).size.width / 12,
+          right: MediaQuery.of(context).size.width / 12),
+      child: Directionality(
+        textDirection: TextDirection.rtl,
+        child: Row(
+          children: [
+            const Icon(Icons.phone_enabled_outlined),
+            const Text("ویرایش شماره موبایل"),
+            Text(" | ", style: TextStyle(fontSize: MediaQuery.of(context).size.width / 20,),),
+            const Icon(Icons.refresh),
+            const Text("درخواست مجدد کد فعالسازی")
+          ],
+        ),
+      ),
+    );
+  }
+}
