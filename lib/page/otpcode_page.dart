58ea5c1e243aed33d0da80487c29aaec7dfacdaf
AliShakoori89 <53476225+AliShakoori89@users.noreply.github.com>
2023-06-28 11:24:04 +0330

    add otp code page

---
+++ lib/page/otpcode_page.dart
@@ -0,0 +1,48 @@
+import 'package:fanar_sign/const/otp_code_text.dart';
+import 'package:flutter/material.dart';
+import 'package:persian_number_utility/persian_number_utility.dart';
+import '../component/main_page_header.dart';
+import '../component/text_field.dart';
+
+class OTPCodePage extends StatefulWidget {
+  const OTPCodePage({super.key, required this.mobileNumberController});
+
+  final int mobileNumberController;
+
+  @override
+  State<OTPCodePage> createState() => _OTPCodePageState(mobileNumberController);
+}
+
+class _OTPCodePageState extends State<OTPCodePage> {
+
+  late TextEditingController otpCodeController = TextEditingController();
+  final int mobileNumberController;
+
+  _OTPCodePageState(this.mobileNumberController);
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          const MainPageHeader(mainPage: false),
+          Container(
+            margin: EdgeInsets.only(
+                left: MediaQuery.of(context).size.width / 20,
+                right: MediaQuery.of(context).size.width / 20),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(height: MediaQuery.of(context).size.height / 4),
+                OTPCodeText(mobileNumberController: mobileNumberController),
+                SizedBox(height: MediaQuery.of(context).size.height / 30),
+                MyTextField(controller: otpCodeController, stringType: false, hintText: "کد احراز هویت", labelText: "کد احراز هویت",),
+
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
