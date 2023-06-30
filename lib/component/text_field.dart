7b224cb92d428ac7a6e3252c0286d455f3b93e2b
alish <alishakoori89@gmail.com>
2023-06-30 23:15:13 +0330

    edit code

--- lib/component/text_field.dart
+++ lib/component/text_field.dart
@@ -8,6 +8,7 @@ class MyTextField extends StatelessWidget {
   final bool stringType;
   final TextEditingController controller;
 
+
   @override
   Widget build(BuildContext context) {
     return Directionality(
@@ -16,25 +17,29 @@ class MyTextField extends StatelessWidget {
           height: MediaQuery.of(context).size.height / 11,
           child: TextField(
             textAlignVertical: TextAlignVertical.center,
-            controller: controller,
-            keyboardType: TextInputType.number,
-            textAlign: TextAlign.center,
-            style: TextStyle(
-                fontSize: MediaQuery.of(context).size.width / 20
-            ),
-            autofocus: true,
-            decoration: InputDecoration(
-              contentPadding: EdgeInsets.only(
-                top: MediaQuery.of(context).size.height / 50,
-                bottom: MediaQuery.of(context).size.height / 50
+              controller: controller,
+              keyboardType: TextInputType.number,
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  fontSize: MediaQuery.of(context).size.width / 20
               ),
+              autofocus: true,
+              decoration: InputDecoration(
+                  // errorText: 'Please enter something',
+                contentPadding: EdgeInsets.only(
+                    top: MediaQuery.of(context).size.height / 50,
+                    bottom: MediaQuery.of(context).size.height / 50,
+                  right: MediaQuery.of(context).size.width / 50,
+                ),
                 border: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(25)
                 ),
+
                 labelText: labelText,
-                hintText: hintText
-            ),
-          ),
+              ),
+              autocorrect: false,
+
+            )
         )
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:device_preview/device_preview.dart';
+import 'package:fanar_sign/page/authentication_page.dart';
 import 'package:fanar_sign/page/authorization_page.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
@@ -28,7 +29,7 @@ class MyApp extends StatelessWidget {
 
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: AuthorizationPage(),
+      home: AuthenticationPage(),
     );
   }
 }
