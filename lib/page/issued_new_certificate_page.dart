5ddbc7109a5e964e371815411c2a4608439b5fdb
alish <alishakoori89@gmail.com>
2023-07-13 18:19:06 +0330

    add issued certificate page

--- lib/page/issued_new_certificate_page.dart
+++ lib/page/issued_new_certificate_page.dart
@@ -1,14 +1,24 @@
+import 'package:fanar_sign/component/dropdown_button.dart';
 import 'package:flutter/material.dart';
 
-class IssuedNewCertificate extends StatelessWidget {
-  const IssuedNewCertificate({super.key});
+import '../component/main_page_header.dart';
 
+class IssuedNewCertificatePage extends StatefulWidget {
+  IssuedNewCertificatePage({super.key});
+
+  @override
+  State<IssuedNewCertificatePage> createState() => _IssuedNewCertificatePageState();
+}
+
+class _IssuedNewCertificatePageState extends State<IssuedNewCertificatePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
         children: [
-
+          const MainPageHeader(mainPage: false),
+          SizedBox(height: MediaQuery.of(context).size.height / 20,),
+          DropDownButton()
         ],
       ),
     );
