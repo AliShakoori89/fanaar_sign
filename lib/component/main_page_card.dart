6e2ffb7b22e9d039409cbcfb672e3553751bc699
AliShakoori89 <53476225+AliShakoori89@users.noreply.github.com>
2023-07-15 14:35:30 +0330

    add gesture detector in main page card

--- lib/component/main_page_card.dart
+++ lib/component/main_page_card.dart
@@ -12,39 +12,47 @@ class MainPageCard extends StatelessWidget {
       flex: 7,
       child: ListView.builder(
           itemBuilder: (BuildContext context, int index){
-            return Directionality(
-              textDirection: TextDirection.rtl,
-              child: Container(
-                height: MediaQuery.of(context).size.height / 12,
-                margin: const EdgeInsets.only(
-                    right: 15,
-                    left: 15,
-                    bottom: 10
-                ),
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(25),
-                  boxShadow: const [
-                    BoxShadow(color: AppColors.cardShadowColor,
-                      spreadRadius: 1,
-                      blurRadius: 4,
-                      offset: Offset(
-                        0.0, // Move to right 5  horizontally
-                        5.0, // Move to bottom 5 Vertically
-                      ),),
-                  ],
-                ),
-                child: Padding(
-                  padding: EdgeInsets.only(
-                    right: MediaQuery.of(context).size.width / 20
+            return GestureDetector(
+              onTap: (){
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => ),
+                );
+              },
+              child: Directionality(
+                textDirection: TextDirection.rtl,
+                child: Container(
+                  height: MediaQuery.of(context).size.height / 12,
+                  margin: const EdgeInsets.only(
+                      right: 15,
+                      left: 15,
+                      bottom: 10
                   ),
-                  child: Align(
-                    alignment: Alignment.centerRight,
-                    child: Text(
-                      cardList[index],
-                      style: TextStyle(color: Colors.black,
-                        fontSize: MediaQuery.of(context).size.width / 25,
-                        fontWeight: FontWeight.w700
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(25),
+                    boxShadow: const [
+                      BoxShadow(color: AppColors.cardShadowColor,
+                        spreadRadius: 1,
+                        blurRadius: 4,
+                        offset: Offset(
+                          0.0, // Move to right 5  horizontally
+                          5.0, // Move to bottom 5 Vertically
+                        ),),
+                    ],
+                  ),
+                  child: Padding(
+                    padding: EdgeInsets.only(
+                      right: MediaQuery.of(context).size.width / 20
+                    ),
+                    child: Align(
+                      alignment: Alignment.centerRight,
+                      child: Text(
+                        cardList[index],
+                        style: TextStyle(color: Colors.black,
+                          fontSize: MediaQuery.of(context).size.width / 25,
+                          fontWeight: FontWeight.w700
+                        ),
                       ),
                     ),
                   ),
