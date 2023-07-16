7acd8e6982ead2ea6751e635a6555d37bbcd3082
AliShakoori89 <53476225+AliShakoori89@users.noreply.github.com>
2023-07-16 09:12:21 +0330

    add activation page

---
+++ lib/page/activation_page.dart
@@ -0,0 +1,18 @@
+import 'package:fanar_sign/component/main_page_header.dart';
+import 'package:flutter/material.dart';
+
+class ActivationCode extends StatelessWidget {
+  const ActivationCode({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          MainPageHeader(mainPage: false),
+
+        ],
+      ),
+    );
+  }
+}
