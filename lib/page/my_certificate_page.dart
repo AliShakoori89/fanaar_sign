76da5557c30b74d3f27c47c57e201f0f3354916c
AliShakoori89 <53476225+AliShakoori89@users.noreply.github.com>
2023-07-16 09:14:16 +0330

    add my certificate page

---
+++ lib/page/my_certificate_page.dart
@@ -0,0 +1,24 @@
+import 'package:fanar_sign/component/floating_action_button.dart';
+import 'package:fanar_sign/component/main_page_card.dart';
+import 'package:fanar_sign/component/main_page_header.dart';
+import 'package:flutter/material.dart';
+
+class MyCertificatePage extends StatelessWidget {
+  MyCertificatePage({super.key});
+
+  List cardName = ["سرویس خدمات 1","سرویس خدمات 2","سرویس خدمات 3","سرویس خدمات 4"];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: MYAppFloatingActionButton(buttonName: "فعالسازی", buttonType:"MyCertificatePage"),
+      body: Column(
+        children: [
+          MainPageHeader(mainPage: false),
+          SizedBox(height: MediaQuery.of(context).size.height / 50,),
+          MainPageCard(cardList: cardName)
+        ],
+      ),
+    );
+  }
+}
